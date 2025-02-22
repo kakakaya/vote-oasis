aa13f9930b96593a4fe00c48ff4aa47627ae99b6
kakakaya <kakakaya+git@gmail.com>
2025-02-22 14:52:06 +0900

    feat: add first page

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let question = "今日の気分は？";
+    let answer = "";
+
+    function submitAnswer() {
+        alert(`回答: ${answer}`);
+        answer = "";
+    }
+</script>
+
+<h1>アンケート</h1>
+<p>{question}</p>
+<input bind:value={answer} placeholder="回答を入力" />
+<button on:click={submitAnswer}>送信</button>
